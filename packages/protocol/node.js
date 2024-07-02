cd48e0144fb06da50204b04115a0fcfae01405cb
yushihang <shihang.yu@hsbc.com>
2024-07-02 18:44:56 +0800

    update node.js

--- packages/protocol/node.js
+++ packages/protocol/node.js
@@ -1,8 +1,15 @@
+const fs = require("fs");
+
 function sleep(ms) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 }
+function capitalizeFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+console.log("name = " + name);
 
 const Web3 = require("web3");
 const web3 = new Web3("http://localhost:8545");
@@ -10,7 +17,7 @@ const provider = new Web3.providers.WebsocketProvider("ws://localhost:8545");
 
 const path = require("path");
 __dirname =
-  "/Users/yushihang/Documents/HSBC/anonymous-zether/packages/protocol";
+  "/Users/yushihang/Documents/HSBC/web3/anonymous-zether/packages/protocol";
 const Client = require(path.join(__dirname, "../anonymous.js/src/client.js"));
 
 const contract = require("@truffle/contract");
@@ -18,11 +25,11 @@ const contract = require("@truffle/contract");
 let ZSCJSON, ZSC, zsc, CashTokenJSON, CashToken, cash, home;
 
 (async () => {
-  console.log("executing...");
+  //console.log("executing...");
   ZSCJSON = require(path.join(__dirname, "build/contracts/ZSC.json"));
   ZSC = contract(ZSCJSON);
   await sleep(1000);
-  console.log("executing 2...");
+  //console.log("executing 2...");
 
   ZSC.setProvider(provider);
   await sleep(1000);
@@ -56,5 +63,69 @@ let ZSCJSON, ZSC, zsc, CashTokenJSON, CashToken, cash, home;
   await sleep(1000);
   cash.approve(zsc.address, 1000, { from: home });
   console.log(cash);
-  console.log("Cash minted and approved");
+
+  eval(
+    `globalThis.${name} = new Client(web3, zsc.contract, home); globalThis.${name}.register();`
+  );
+  await sleep(1000);
+
+  console.log("public key:");
+  publicKey = eval(`globalThis.${name}.account.public();`);
+  publicKeyStr = JSON.stringify(publicKey);
+  console.log(publicKeyStr);
+
+  eval(`globalThis.${name}.deposit(100);`);
+
+  await sleep(1000);
+
+  const dirPath = "./pubkey";
+  const filePath = path.join(dirPath, `${capitalizeFirstLetter(name)}.json`);
+
+  const content = publicKeyStr;
+
+  try {
+    fs.writeFileSync(filePath, content);
+    console.log("Write public key to file success!");
+  } catch (err) {
+    console.error("Write public key to file failed:", err);
+  }
+
+  await sleep(1000);
+  console.log("account created for " + name);
+
+  await sleep(3000);
+
+  try {
+    const fileNames = fs.readdirSync(dirPath);
+
+    const jsonFiles = fileNames.filter((fileName) =>
+      fileName.endsWith(".json")
+    );
+
+    console.log(jsonFiles);
+
+    for (const fileName of jsonFiles) {
+      const filePath = path.join(dirPath, fileName);
+      const fileContent = fs.readFileSync(filePath, "utf8");
+      const pubkey = JSON.parse(fileContent);
+      if (
+        Array.isArray(pubkey) &&
+        pubkey.every((item) => typeof item === "string")
+      ) {
+        const friendsName = path.basename(fileName, ".json");
+        if (friendsName.toLowerCase() !== name.toLowerCase()) {
+          eval(
+            `globalThis.${name}.friends.add("${friendsName}", ${fileContent});`
+          );
+          console.log(`add friend ${friendsName} with pubkey ${fileContent}`);
+        }
+      } else {
+        console.warn(` ${fileName} content format error`);
+      }
+    }
+
+    console.log("load friends pubkeys success");
+  } catch (err) {
+    console.error("load friends pubkeys failed", err);
+  }
 })();
--- packages/protocol/node.sh
+++ packages/protocol/node.sh
@@ -1,1 +1,34 @@
-node -i -e "$(cat ./node.js)"
+#!/bin/bash
+script_dir=$(dirname "$(realpath "$0")")
+echo "Current script path: $script_dir"
+
+rm -rf "$script_dir/pubkey"
+mkdir "$script_dir/pubkey"
+
+cd "$script_dir"
+sleep 1
+#npm install -g iterm2-tab-set
+#brew tap mklement0/ttab https://github.com/mklement0/ttab.git
+#brew install mklement0/ttab/ttab
+
+#npm install -g truffle
+#npm install -g ganache
+yarn
+
+
+ttab 'tabset -a 'ganache' -c purple; ganache-cli --gasPrice 0 -k berlin'
+sleep 2
+
+ttab 'truffle migrate; exit'
+sleep 2
+
+names=("alice" "bob" "carol" "dave" "miner")
+colors=("red" "blue" "yellow" "green" "orange")
+
+for i in "${!names[@]}"; do
+    name="${names[i]}"
+    color="${colors[i]}"
+    ttab "tabset -a '$name' -c '$color'; node -i -e \"const name='$name'; \$(cat ./node.js)\";"
+done
+
+exit
